e19b108771a1d946dab38773ef2655cc14f38b33
RaulGarviCascos <75993527+RaulGarviCascos@users.noreply.github.com>
2024-10-31 18:49:44 +0000

    fixed

--- merge-sorted-array/merge-sorted-array.py
+++ merge-sorted-array/merge-sorted-array.py
@@ -9,26 +9,33 @@ def merge(nums1, m, nums2, n):
     :rtype: None Do not return anything, modify nums1 in-place instead.
     """
     actualPosNum2 = n - 1
+    
     if m == 0:
         for i in range(n):
             nums1[i] = nums2[i]
     else:
         while actualPosNum2 >= 0:
             actualPosNum1 = len(nums1) -1
-            while nums1[actualPosNum1] == 0:
+            itsEmptyPosition = True
+            while nums1[actualPosNum1] == 0 and actualPosNum1 >= 0:
                 if actualPosNum1 - 1 >= 0 and nums1[actualPosNum1 - 1] > nums2[actualPosNum2]:
                     nums1[actualPosNum1] = nums1[actualPosNum1 - 1]
                     nums1[actualPosNum1 - 1] = 0
-                elif nums1[actualPosNum1 - 1] != 0:
+                    itsEmptyPosition = False
+                elif  not itsEmptyPosition or nums1[actualPosNum1 - 1] != 0  and itsEmptyPosition:
                     nums1[actualPosNum1] = nums2[actualPosNum2]
+                    break
+                
                 actualPosNum1 -= 1
             actualPosNum2 -= 1
 
-nums1 = [1,2,3,0,0,0]
-m=3
-nums2 = [2,5,6]
-n=3
+nums1 = [-1,0,0,3,3,3,0,0,0] 
+m=6
+'''nums1 = [4, 5, 6, 0, 0, 0]
 
+m= 3'''
+nums2 = [1,2,3]
+n=3
 print(nums1)
 print(nums2)
 merge(nums1,m,nums2,n)
